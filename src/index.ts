a0a56ce37bf65c249c5203eb87a3f2cc4c88372e
Ljupche Vasilev <ljupche.vasilev@gmail.com>
2024-01-21 22:44:32 +0000

    Validate CSV

--- src/index.ts
+++ src/index.ts
@@ -1,45 +1,36 @@
 import Koa from "koa";
 import Router from "@koa/router";
 import koaBody from "koa-body";
+import { parseCsv, validate } from "csval";
 
 const app = new Koa();
 const router = new Router();
 
 app.use(koaBody());
 
-router.post("/", async (ctx) => {
-    const body = ctx.request.body;
-
-    const data: { errors?: string[], message?: string } = {errors: []}
-    // validate email
-    if (!body.email) {
-        data.errors = []
-        data.errors.push("Email is required")
-    } else if (typeof body.email !== "string" || body.email instanceof String) {
-        data.errors?.push("Email must be a string")
+router.post("/", async (ctx, next) => {
+    const rules = {
+        "properties": {
+            "name": {
+                "type": "string"
+            },
+            "job": {
+                "type": "string"
+            },
+            "age": {
+                "type": "number"
+            }
+        },
+        "required": ["name", "job", "age"]
+    };
+    const parsed = await parseCsv(ctx.request.body);
+    try {
+        const valid = await validate(parsed, rules);
+        ctx.body = { valid };
+    } catch (e) {
+        ctx.body = { valid: false, error: e.message };
+        return
     }
-
-    // validate password
-    if (!body.password) {
-        data.errors?.push("Password is required")
-    } else if ( typeof body.password !== "string" || body.password instanceof String) {
-        data.errors?.push("Password must be a string")
-    }
-
-    // validate username
-    if (!body.username) {
-        data.errors?.push("Username is required")
-    } else if (typeof body.username !== "string" || body.username instanceof String) {
-        data.errors?.push("Username must be a string")
-    }
-
-    if (!data.errors || data.errors.length === 0) {
-        delete data.errors
-        data.message = "User created successfully"
-    } else {
-        data.message = "User creation failed"
-    }
-    ctx.body = { data };
 });
 
 app.use(router.routes());
